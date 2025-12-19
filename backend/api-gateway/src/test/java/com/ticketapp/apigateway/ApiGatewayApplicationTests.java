79f21838356d5ac3c9f8c0d31fb71e8c4aef80d0
Yajnesh Kharayat <74180255+yajji-k@users.noreply.github.com>
2025-12-20 00:41:11 +0530

    added api gateway and eureka service registry and connected authms to the gateway

---
+++ backend/api-gateway/src/test/java/com/ticketapp/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticketapp.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ backend/service-registry/src/test/java/com/ticketapp/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticketapp.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
